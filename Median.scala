670302cf0a362ccc4c6c2076a999133f6114124d
Yanjie Gao <gaoyanjie55@163.com>
2014-08-15 20:58:12 +0800

    求中位数
    海量数据求中位数

---
+++ Median.scala
@@ -0,0 +1,37 @@
+import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.SparkContext.rddToPairRDDFunctions
+/**
+ * Created by v-yanjga on 8/15/2014.
+ */
+object Median {
+  def main(args: Array[String]) {
+    //val data=Seq(1,1,2,2,3,34,5,6,7,8,86,8,7,97,9,7,97,9,78,978)
+    val conf = new SparkConf().setAppName("Spark Pi")
+    val spark = new SparkContext(conf)
+    val n = 10000
+    val data = spark.parallelize(1 to n, slices)
+    val mappeddata =data.map(num => {
+      (num/1000 , num)
+    })
+    val count = mappeddata.reduceByKey((a , b) => {
+      a+b
+    }).collect()
+    val sum_count=count.map(data => {
+      data._2
+    }).sum
+    var temp = 0
+    var index = 0
+    var mid = sum_count/2
+    for( i <- 0 to 10) {
+      temp=temp+count(i)
+      if(temp >= mid) {
+        index=i
+        break
+      }
+    }
+    val offset = temp - mid
+    val result = mappeddata.filter(num => num._1 == index ).takeOrdered(offset)
+    println("Median is " + result )
+    spark.stop()
+  }
+}
