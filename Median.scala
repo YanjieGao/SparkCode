c3ea2c289ae6c03625618e749d1bac880329fb7f
Yanjie Gao <gaoyanjie55@163.com>
2014-08-16 14:19:17 +0800

    海量数据求中位数

--- Median.scala
+++ Median.scala
@@ -9,12 +9,15 @@ object Median {
     val spark = new SparkContext(conf)
     val n = 10000
     val data = spark.parallelize(1 to n, slices)
+    //将数据逻辑划分为10个桶，统计每个桶中落入的数据量
     val mappeddata =data.map(num => {
       (num/1000 , num)
     })
     val count = mappeddata.reduceByKey((a , b) => {
       a+b
     }).collect()
+    //根据总的数据量，通过逐次从桶序号由低到高依次累加，判断中位数落在哪个
+    //桶中，并且获取到中位数在桶中的偏移量
     val sum_count=count.map(data => {
       data._2
     }).sum
@@ -28,7 +31,9 @@ object Median {
         break
       }
     }
+    //中位数在桶中偏移量
     val offset = temp - mid
+    //获取到中位数所在桶中的的偏移量为offset的数，也就是中位数
     val result = mappeddata.filter(num => num._1 == index ).takeOrdered(offset)
     println("Median is " + result )
     spark.stop()
